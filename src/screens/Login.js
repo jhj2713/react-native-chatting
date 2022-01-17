73b7d152c33b4f25e9956662c4b4b8f5fb3bf3c9
juhyojeong <juhyojeong58@gmail.com>
2022-01-17 13:36:21 +0900

    Add: 에러 메시지 표시

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import styled from "styled-components/native";
 import { Image, Input } from "../components";
 import { images } from "../utils/images";
+import { validateEmail, removeWhitespace } from "../utils/common";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const Container = styled.View`
@@ -11,11 +12,32 @@ const Container = styled.View`
   background-color: ${({ theme }) => theme.background};
   padding: 20px;
 `;
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 10px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.errorText};
+`;
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   const passwordRef = useRef();
+
+  const _handleEmailChange = (email) => {
+    const changedEmail = removeWhitespace(email);
+    setEmail(changedEmail);
+    setErrorMessage(
+      validateEmail(changedEmail) ? "" : "Please verify your email",
+    );
+  };
+  const _handlePasswordChange = (password) => {
+    setPassword(removeWhitespace(password));
+  };
 
   return (
     <KeyboardAwareScrollView
@@ -27,7 +49,7 @@ const Login = ({ navigation }) => {
         <Input
           label="Email"
           value={email}
-          onChangeText={(text) => setEmail(text)}
+          onChangeText={_handleEmailChange}
           onSubmitEditing={() => {
             passwordRef.current.focus();
           }}
@@ -38,11 +60,12 @@ const Login = ({ navigation }) => {
           ref={passwordRef}
           label="Password"
           value={password}
-          onChangeText={(text) => setPassword(text)}
+          onChangeText={_handlePasswordChange}
           onSubmitEditing={() => {}}
           placeholder="Password"
           returnKeyType="next"
         />
+        <ErrorText>{errorMessage}</ErrorText>
       </Container>
     </KeyboardAwareScrollView>
   );
