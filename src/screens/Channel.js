703cad0e2d78c79d7d788ae7e1f1fd574f9cdc79
juhyojeong <juhyojeong58@gmail.com>
2022-01-17 19:24:18 +0900

    Add: 채널 내 message 불러오기 로직

--- src/screens/Channel.js
+++ src/screens/Channel.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { firestore } from "../utils/firebase";
 import styled from "styled-components/native";
-import { Text } from "react-native";
+import { Text, FlatList } from "react-native";
 
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
 `;
 
-const Channel = ({ route: { params } }) => {
+const Channel = ({ navigation, route: { params } }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firestore
+      .collection("channels")
+      .doc(params.id)
+      .collection("messages")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const list = [];
+        snapshot.forEach((doc) => {
+          list.push(doc.data());
+        });
+        setMessages(list);
+      });
+    return () => unsubscribe();
+  }, []);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerTitle: params.title || "Channel" });
+  }, []);
+
   return (
     <Container>
-      <Text style={{ fontSize: 24 }}>ID: {params?.id}</Text>
-      <Text style={{ fontSize: 24 }}>TITLE: {params?.title}</Text>
+      <FlatList
+        keyExtractor={(item) => item["id"]}
+        data={messages}
+        renderItem={({ item }) => (
+          <Text style={{ fontSize: 24 }}>{item.text}</Text>
+        )}
+      />
     </Container>
   );
 };
