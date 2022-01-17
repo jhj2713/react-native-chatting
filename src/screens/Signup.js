483cfbaece8d1f7364a5cfb63ee7d5c4f94c16b2
juhyojeong <juhyojeong58@gmail.com>
2022-01-17 14:23:44 +0900

    Add: 기본 프로필 사진 추가

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -3,6 +3,7 @@ import styled from "styled-components/native";
 import { Image, Input, Button } from "../components";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { validateEmail, removeWhitespace } from "../utils/common";
+import { images } from "../utils/images";
 
 const Container = styled.View`
   flex: 1;
@@ -27,6 +28,7 @@ const Signup = () => {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [photoUrl, setPhotoUrl] = useState(images.photo);
 
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -63,7 +65,7 @@ const Signup = () => {
   return (
     <KeyboardAwareScrollView extraScrollHeight={20}>
       <Container>
-        <Image rounded />
+        <Image url={photoUrl} rounded />
         <Input
           label="Name"
           value={name}
