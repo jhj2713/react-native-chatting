a73f035fc04c903bc86a338854f7c1fdd732fa39
juhyojeong <juhyojeong58@gmail.com>
2022-01-17 16:40:36 +0900

    Add: 회원가입 로직 구현

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,55 @@
 import firebase from "firebase/compat/app";
 import config from "../../firebase.json";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 firebase.initializeApp(config);
 
 const auth = getAuth();
+const storage = getStorage();
 
 export const login = async ({ email, password }) => {
   const { user } = await signInWithEmailAndPassword(auth, email, password);
   return user;
+};
+
+export const signup = async ({ email, password, name, photoUrl }) => {
+  const { user } = await createUserWithEmailAndPassword(auth, email, password);
+  const storageUrl = photoUrl.startsWith("https")
+    ? photoUrl
+    : await uploadImage(photoUrl);
+  await updateProfile(auth.currentUser, {
+    displayName: name,
+    photoUrl: storageUrl,
+  });
+  return user;
+};
+
+const uploadImage = async (uri) => {
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function (e) {
+      reject(new TypeError("Network request failed"));
+    };
+    xhr.responseType = "blob";
+    xhr.open("GET", uri, true);
+    xhr.send(null);
+  });
+
+  const user = auth.currentUser;
+  const imageRef = ref(storage, `/profile/${user.uid}/photo.png`);
+  const snapshot = await uploadBytes(imageRef, blob, {
+    contentType: "image/png",
+  });
+
+  blob.close();
+  return await getDownloadURL(imageRef);
 };
